7c282158a128a5a5fa12c7433c0b3853dcc0d4b9
“linhPham98” <linhthusinh98@gmail>
2024-11-14 00:46:55 +0900

    add icon and color

--- src/Card/CardComponent.jsx
+++ src/Card/CardComponent.jsx
@@ -2,14 +2,13 @@ import { Card, Col, Row, Space } from 'antd';
 import { useState } from 'react';
 
 const CardComponent = () => {
-
   const currentDateTime = new Date();
   const formattedDateTime = `${currentDateTime.getDate()}/${currentDateTime.getMonth() + 1}/${currentDateTime.getFullYear()} ${currentDateTime.getHours()}:${currentDateTime.getMinutes()}:${currentDateTime.getSeconds()}`;
-  const [done, setDone] = useState('Chưa thực hiện');
+  const [done, setDone] = useState(false);
   const [color, setColor] = useState('');
   const [time, setTime] = useState('');
   const changeToDone = () => {
-    setDone('Đã thực hiện');
+    setDone(true);
     setColor('green');
     setTime(':'+formattedDateTime);
   }
@@ -17,68 +16,68 @@ const CardComponent = () => {
   return  <Space direction="vertical" size="large" style={{ width: '100%' }}>
   <Row gutter={[16, 16]}>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Đồ ăn" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-        {done} {time}
+      <Card title="Mua Đồ ăn" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+        {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Nước uống" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-      {done}
+      <Card title="Mua Nước uống" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+     {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Giấy vệ sinh" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-      {done}
+      <Card title="Mua Giấy vệ sinh" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+     {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Đũa" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-      {done}
+      <Card title="Mua Đũa" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+     {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Kem đánh răng" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      <Card title="Mua Kem đánh răng" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Túi rác" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      <Card title="Mua Túi rác" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
       <Card title="Đóng tiền điện" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
       <Card title="Đóng tiền gas" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Nước giặt" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      <Card title="Mua Nước giặt" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title="Dầu gội" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      <Card title="Mua Dầu gội" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
-      <Card title=" Dầu rửa bát" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      <Card title="Mua Dầu rửa bát" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
       <Card title="Đóng tiền học" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
     <Col xs={24} sm={12} md={8}>
       <Card title="Đóng tiền điện thoại" bordered={true} style={{backgroundColor:color}} onClick={changeToDone}>
-       {done}
+      {done ? 'Đã thực hiện' : 'Chưa thực hiện'} {time}
       </Card>
     </Col>
   </Row>
